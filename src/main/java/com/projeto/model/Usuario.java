8e8af33ba40c79e460a29233755a43f789714f79
João Henrique da Silva <joaohenriquedasilva244@gmail.com>
2026-05-15 09:06:14 -0300

    feat: implementa metodos de busca e salvamento no repository

--- src/main/java/com/projeto/model/Usuario.java
+++ src/main/java/com/projeto/model/Usuario.java
@@ -1,5 +1,40 @@
-package com.projeto.model;
+package com.projeto.repository;
 
-public class Usuario {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.projeto.model.Usuario;
+
+
+
+public class UsuarioRepository {
+    private static List<Usuario> bancoDeDados = new ArrayList<>();
     
+    public void salvar(Usuario usuario){
+        bancoDeDados.add(usuario);
+    }
+    public List<Usuario> Listartodos(){
+        return bancoDeDados;
+    }
+    public Optional<Usuario> buscarPorCpf(String cpf) {
+        for (Usuario u : bancoDeDados) {
+            if (u.getCpf().equals(cpf)) {
+                return Optional.of(u);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Usuario> buscarPorEmail(String email) {
+        for (Usuario u : bancoDeDados) {
+            if (u.getEmailInstitucional().equals(email)) {
+                return Optional.of(u);
+            }
+        }
+        return Optional.empty();
+    }
+
+
+
 }
